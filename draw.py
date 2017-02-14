83df2d01011c0590217dd78cfb804766d86ab0cb
Billy Wong <bwong5@stuy.edu>
2017-02-14 15:35:34 -0500

    does it work?

--- draw.py
+++ draw.py
@@ -1,10 +1,10 @@
+from __future__ import division
 from display import *
-    
 
 # Bresenham algorithm
 # Works only for octant 1
 
-def draw_line( screen, x0, y0, x1, y1, color ):
+def lineO1( screen, x0, y0, x1, y1, color ):
     x = x0
     y = y0
 
@@ -20,35 +20,84 @@ def draw_line( screen, x0, y0, x1, y1, color ):
             y+=1
             d+= 2*B
         d += 2*A
-        
-
-#----------------------------------------------
-
-#Returns a list with slope and y-intercept 
-#adjusted so that the line works with ppm file writing
-def equation4Pic(x0,y0,x1,y1):
-    data = []
-    data.append( ( (y1-y0) *1.0) /(x1-x0))
-    data.append(y0-x0*data[0])
-    return data
-
-def onEquat(l,x,y):
-    actY = l[0]*x+l[1]
-    if actY == 0:
-        return y == 0
-    diff = abs(actY-y)*100/actY
-    return diff < 0.5
 
 
-# My own line equation
-# Works for any line
-# Not too efficient
-# Works pretty bad for lines with slopes close to 0
+# x+1/2, y+1
+def lineO2(screen,x0,y0,x1,y1,color):
+    x = x0
+    y = y0
 
-def draw_line_own( screen, x0, y0, x1, y1, color ):
-    es = equation4Pic(x0,y0,x1,y1)
+    A = y1-y0
+    B = -(x1-x0)
     
-    for y in range(y0,y1+1):
-        for x in range(x0,x1+1):
-            if onEquat(es,x,y):
-                plot(screen,color,x,y)
+    d = A+2*B
+    
+    while y <= y1:
+        plot(screen,color,x,y)
+	y += 1
+	if d < 0:
+	    x += 1
+	    d += 2*A
+	d += 2*B
+
+
+# x+1/2 , y-1
+def lineO7(screen,x0,y0,x1,y1,color):
+    x = x0
+    y = y0
+
+    A = y1-y0
+    B = -(x1-x0)
+
+    d = A-2*B
+
+    while y > y1:
+        plot(screen,color,x,y)
+        y -= 1
+        d -= 2*B
+        if d > 0:
+            x+=1
+            d += 2*A
+
+# x+1, y-1/2
+def lineO8(screen,x0,y0,x1,y1,color):
+    x = x0
+    y = y0
+
+    A = y1-y0
+    B = -(x1-x0)
+
+    d = 2*A-B
+
+    while x < x1:
+        plot(screen,color,x,y)
+        x += 1
+        d -= 2*A
+        if d < 0:
+            y-=1
+            d += 2*B
+
+
+def draw_line( screen, x0, y0, x1, y1, color ):
+    if x0 > x1:
+        tempX = x0
+        tempY = y0
+        x0 = x1
+        y0 = y1
+        x1 = tempX
+        y1 = tempY
+
+    slope = (y1-y0)/(x1-x0)
+
+    if slope > 0 and slope < 1:
+        lineO1(screen,x0,y0,x1,y1,color)
+    elif slope > 1:
+        lineO2(screen,x0,y0,x1,y1,color)
+    elif slope < 0 and slope > -1:
+        lineO8(screen,x0,y0,x1,y1,color)
+    elif slope < -1:
+        lineO7(screen,x0,y0,x1,y1,color)
+    else:
+        pass #ERROR
+
+    
--- main.py
+++ main.py
@@ -6,14 +6,10 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 
 
-'''
-display(screen)
-save_extension(screen, 'img.png')
-
-'''
 
 #Don't call display_name and save_extension while the image is open
 
+'''
 x0 = 0
 y0 = 0
 x1 = 500
@@ -27,24 +23,12 @@ for y1 in range(0,500,10):
     color[2] = color[2]%256
     
     draw_line(screen,x0,y0,x1,y1,color)
-
+    
 save_extension(screen, 'img.png')
 display(screen)
-
 '''
-clear_screen(screen)
 
-for y1 in range(0,500,10):
-    color[0] += random.randint(0,255)
-    color[1] += random.randint(0,255)
-    color[2] += random.randint(0,255)
-    color[0] = color[0]%256
-    color[1] = color[1]%256
-    color[2] = color[2]%256
-    
-    draw_line_own(screen,x0,y0,x1,y1,color)
-    
-save_extension(screen, 'imgme.png')
-display_name(screen,'imgme.ppm')
 
-'''
+draw_line(screen,400,500,120,4,color)
+save_extension(screen, 'img.png')
+display(screen)
